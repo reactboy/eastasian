bea9a510d832f7c8d6902f963e6a99528976edac
eastasian <junceliz@gmail.com>
2022-10-16 12:13:09 +0800

    edit configure global style for resume.eastasian

--- apps/eastasian/pages/_app.tsx
+++ apps/eastasian/pages/_app.tsx
@@ -1,18 +1,35 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import './styles.css';
+import { Global, css } from '@emotion/react';
 
-function CustomApp({ Component, pageProps }: AppProps) {
+// NOTE(eastasian) reset css
+import 'sanitize.css';
+import 'sanitize.css/typography.css';
+import 'sanitize.css/forms.css';
+import 'sanitize.css/assets.css';
+
+import { COLOR } from '@resume/libs/styles';
+
+const baseCss = css`
+  body {
+    font-family: '"Noto Sans JP", sans-serif';
+    color: ${COLOR['text']};
+  }
+`;
+
+function Eastasian({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Welcome to eastasian!</title>
+        <title>Jun Aida / eastasian</title>
       </Head>
-      <main className="app">
+      <Global styles={baseCss} />
+      <main>
         <Component {...pageProps} />
+        <input type="text" />
       </main>
     </>
   );
 }
 
-export default CustomApp;
+export default Eastasian;
