86e27b156b8a7cc06f1bdc1a9e47c9aa00b28951
eastasian <junceliz@gmail.com>
2022-10-16 13:36:17 +0800

    add prepare libs and components dir for resume.eastasian

---
+++ apps/eastasian/libs/styles/color.ts
@@ -0,0 +1,10 @@
+export const COLOR = {
+  primary: '#3DA9FC',
+  sedondary: '#90B4CE',
+  tertiary: '#EF4565',
+  dark: '#094067',
+  light: '#FFFFFE',
+  gray: '#E4E4E4',
+  heading: '#094067',
+  text: '#094067',
+};
